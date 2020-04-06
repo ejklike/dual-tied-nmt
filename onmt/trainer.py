c9943fa9d44c8202cec1e4291b92d5fc495ba893
Eunji <eunjikim@dm.snu.ac.kr>
2020-04-06 10:36:58 +0900

    Update trainer.py

--- onmt/trainer.py
+++ onmt/trainer.py
@@ -401,12 +401,14 @@ class Trainer(object):
 
         enc, mem, _ = self.model.encoder(x, xlen)
         input_, target = y[:-1], y[1:]
-
+        
+        # hard selection loss
         dec_in = self.get_dec_in(input_, winners)
         loss, stat_dict = self._get_loss_y(
             dec_in, target, enc, mem, x, xlen, side=side, 
             reduced_sum=True)
         
+        # add prior loss to the loss above, if available
         if self.learned_prior:
             prior = (self.model.prior_x2y if side == 'x2y' else 
                      self.model.prior_y2x)
@@ -620,41 +622,3 @@ class Trainer(object):
         if self.source_noise is not None:
             return self.source_noise(batch)
         return batch
-
-
-class LogSumExpMoE(torch.autograd.Function):
-    """Standard LogSumExp forward pass, but use *posterior* for the backward.
-    See `"Mixture Models for Diverse Machine Translation: Tricks of the Trade"
-    (Shen et al., 2019) <https://arxiv.org/abs/1902.07816>`_.
-    """
-
-    @staticmethod
-    def forward(ctx, logp, posterior, dim=-1):
-        ctx.save_for_backward(posterior)
-        ctx.dim = dim
-        return torch.logsumexp(logp, dim=dim)
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        posterior, = ctx.saved_tensors
-        grad_logp = grad_output.unsqueeze(ctx.dim) * posterior
-        return grad_logp, None, None
-
-
-class LogSumExpMoE(torch.autograd.Function):
-    """Standard LogSumExp forward pass, but use *posterior* for the backward.
-    See `"Mixture Models for Diverse Machine Translation: Tricks of the Trade"
-    (Shen et al., 2019) <https://arxiv.org/abs/1902.07816>`_.
-    """
-
-    @staticmethod
-    def forward(ctx, logp, posterior, dim=-1):
-        ctx.save_for_backward(posterior)
-        ctx.dim = dim
-        return torch.logsumexp(logp, dim=dim)
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        posterior, = ctx.saved_tensors
-        grad_logp = grad_output.unsqueeze(ctx.dim) * posterior
-        return grad_logp, None, None
