aadce2b9c04958ec87a43cc753e3a349eabdf8ac
Eunji <eunjikim@dm.snu.ac.kr>
2020-04-08 20:23:53 +0900

    add two-way priors and decoders

--- onmt/bin/average_models.py
+++ onmt/bin/average_models.py
@@ -13,17 +13,30 @@ def average_models(model_files, fp32=False):
         m = torch.load(model_file, map_location='cpu')
         model_weights = m['model']
         generator_weights = m['generator']
+        prior_x2y_weights = prior_y2x_weights = None
+        if m.get('prior_x2y', None):
+            prior_x2y_weights = m['prior_x2y']
+        if m.get('prior_y2x', None):
+            prior_y2x_weights = m['prior_y2x']
 
         if fp32:
             for k, v in model_weights.items():
                 model_weights[k] = v.float()
             for k, v in generator_weights.items():
                 generator_weights[k] = v.float()
+            if prior_x2y_weights is not None:
+                for k, v in prior_x2y_weights.items():
+                    prior_x2y_weights[k] = v.float()
+            if prior_y2x_weights is not None:
+                for k, v in prior_y2x_weights.items():
+                    prior_y2x_weights[k] = v.float()
 
         if i == 0:
             vocab, opt = m['vocab'], m['opt']
             avg_model = model_weights
             avg_generator = generator_weights
+            avg_prior_x2y = prior_x2y_weights if prior_x2y_weights else None
+            avg_prior_y2x = prior_y2x_weights if prior_y2x_weights else None
         else:
             for (k, v) in avg_model.items():
                 avg_model[k].mul_(i).add_(model_weights[k]).div_(i + 1)
@@ -31,8 +44,19 @@ def average_models(model_files, fp32=False):
             for (k, v) in avg_generator.items():
                 avg_generator[k].mul_(i).add_(generator_weights[k]).div_(i + 1)
 
+            if prior_x2y_weights is not None:
+                for (k, v) in avg_prior_x2y.items():
+                    avg_prior_x2y[k].mul_(i).add_(prior_x2y_weights[k])\
+                        .div_(i + 1)
+            
+            if prior_y2x_weights is not None:
+                for (k, v) in avg_prior_y2x.items():
+                    avg_prior_y2x[k].mul_(i).add_(prior_y2x_weights[k])\
+                        .div_(i + 1)
+
     final = {"vocab": vocab, "opt": opt, "optim": None,
-             "generator": avg_generator, "model": avg_model}
+             "generator": avg_generator, "model": avg_model,
+             "prior_x2y": avg_prior_x2y, "prior_y2x": avg_prior_y2x}
     return final
 
 
